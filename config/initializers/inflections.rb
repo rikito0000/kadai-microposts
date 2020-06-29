546d79d6180a406f08127ce3794065035e208eeb
EC2 Default User <ec2-user@ip-172-31-61-122.ec2.internal>
2020-06-29 05:06:03 +0000

    set preparation

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -14,3 +14,11 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym 'RESTful'
 # end
+
+Rails.application.config.generators do |g|
+  g.stylesheets false
+  g.javascripts false
+  g.helper false
+  g.skip_routes true
+end
+
